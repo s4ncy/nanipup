11cb7334f8c80fe807351abaca6b1ea6cf5a1005
s4ncy <sania.despotovic1@gmail.com>
2026-09-07 10:36:30 +0200

    Upload complete Nani Pup website

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const site = {
+  name: 'Nani Pup',
+  url: 'https://nanipup.eu',
+  email: 'hello@nanipup.eu',
+  instagram: '#',
+  areas: ['Kranjska Gora', 'Lake Bled', 'Bohinj', 'Gorenjska', 'Ljubljana', 'Slovenia']
+};
